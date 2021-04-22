51b1c7cfa89901032f06e3baa2b8633f7b34e47c
Ebin Paul <ebinpaul@outlook.com>
2021-04-22 09:24:05 +0530

    settings update

--- btre/settings.py
+++ btre/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS')]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'listings',
     'realtors',
     'accounts',
+    'contacts',
 ]
 
 MIDDLEWARE = [
