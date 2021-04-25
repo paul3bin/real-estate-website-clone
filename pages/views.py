5b9b55c141e1143a19d8b04e5ff2881708f8cb62
Ebin Paul <ebinpaul@outlook.com>
2021-04-25 09:57:06 +0530

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.choices import price_choices, bedroom_choices, state_choices
+from listings.models import Listing
+from realtors.models import Realtor
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+    return render(request, 'pages/index.html', context={
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    })
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtor = Realtor.objects.filter(is_mvp=True)
+    return render(request, 'pages/about.html', context={
+        'realtors': realtors,
+        'mvp': mvp_realtor
+    })
