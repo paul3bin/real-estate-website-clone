1ae83d8ec66d0ceae55488a1574a4c125c7384d3
Ebin Paul <ebinpaul@outlook.com>
2022-09-02 10:59:15 +0530

    added custom management command to wait for DB to load

---
+++ accounts/management/commands/wait_for_db.py
@@ -0,0 +1,26 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """
+    Django management command to pause execution until the database is up and running.
+    """
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Waiting for database...")
+
+        db_conn = None
+
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+
+            except OperationalError:
+                self.stdout.write("Database unavailable, waiting for 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available!"))
